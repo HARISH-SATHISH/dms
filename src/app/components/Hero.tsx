d597836a9806858fc779917966e9fe268c89a1e9
HARISH-SATHISH <gowdaharish580@gmail.com>
2024-02-15 21:55:06 +0530

    2nd commit

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -13,7 +13,7 @@ const Hero = () => {
       <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Welcome to farmer friendly dairy
         
       </h1>
-      <p className="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
+      <p className="mb-8 leading-relaxed">The farmer-friendly dairy prioritizes the welfare of its farmers by ensuring fair prices for their produce and providing support for sustainable agricultural practices. Through transparent policies and cooperative initiatives, it fosters a mutually beneficial relationship, empowering farmers economically while promoting environmental stewardship. It serves as a model for ethical and inclusive dairy farming.</p>
       <div className="flex justify-center">
         <button className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">Button</button>
         <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Button</button>
--- src/app/components/Price.tsx
+++ src/app/components/Price.tsx
@@ -12,29 +12,23 @@ const Price = () => {
       <table className="table-auto w-full text-left whitespace-no-wrap">
         <thead>
           <tr>
-            <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">Plan</th>
-            <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">Speed</th>
-            <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">Storage</th>
-            <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">Price</th>
-            <th className="w-10 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br"></th>
+            <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">Quality</th>
+            <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">Price/liter</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td className="px-4 py-3">Start</td>
-            <td className="px-4 py-3">5 Mb/s</td>
-            <td className="px-4 py-3">15 GB</td>
-            <td className="px-4 py-3 text-lg text-gray-900">Free</td>
+            <td className="px-4 py-3">C-9</td>
+            <td className="px-4 py-3">34</td>
             
           </tr>
-          <tr>
-            <td className="border-t-2 border-gray-200 px-4 py-3">Pro</td>
-            <td className="border-t-2 border-gray-200 px-4 py-3">25 Mb/s</td>
-            <td className="border-t-2 border-gray-200 px-4 py-3">25 GB</td>
-            <td className="border-t-2 border-gray-200 px-4 py-3 text-lg text-gray-900">$24</td>
+           <tr>
+            <td className="border-t-2 border-gray-200 px-4 py-3">c-8</td>
+            <td className="border-t-2 border-gray-200 px-4 py-3">32</td>
+        
             
           </tr>
-          <tr>
+         {/* <tr>
             <td className="border-t-2 border-gray-200 px-4 py-3">Business</td>
             <td className="border-t-2 border-gray-200 px-4 py-3">36 Mb/s</td>
             <td className="border-t-2 border-gray-200 px-4 py-3">40 GB</td>
@@ -47,7 +41,7 @@ const Price = () => {
             <td className="border-t-2 border-b-2 border-gray-200 px-4 py-3">120 GB</td>
             <td className="border-t-2 border-b-2 border-gray-200 px-4 py-3 text-lg text-gray-900">$72</td>
             
-          </tr>
+          </tr> */}
         </tbody>
       </table>
     </div>

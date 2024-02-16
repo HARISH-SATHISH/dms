f894b56c8a76e1f88bf8e604f94192c2988df1dd
HARISH-SATHISH <gowdaharish580@gmail.com>
2024-02-16 07:54:49 +0530

    3rd commit

--- src/app/components/Price.tsx
+++ src/app/components/Price.tsx
@@ -6,7 +6,7 @@ const Price = () => {
   <div className="container px-5 py-24 mx-auto">
     <div className="flex flex-col text-center w-full mb-20">
       <h1 className="sm:text-4xl text-3xl font-medium title-font mb-2 text-gray-900">Pricing</h1>
-      <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Banh mi cornhole echo park skateboard authentic crucifix neutra tilde lyft biodiesel artisan direct trade mumblecore 3 wolf moon twee</p>
+      <p className="lg:w-2/3 mx-auto leading-relaxed text-base">pricing is subjected to market</p>
     </div>
     <div className="lg:w-2/3 w-full mx-auto overflow-auto">
       <table className="table-auto w-full text-left whitespace-no-wrap">
